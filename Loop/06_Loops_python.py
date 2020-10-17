3060ce4f872dcf66c281b3e2a5e8689b52e0ed43
Ajeya Nayak <ajeyanayak@gmail.com>
2020-10-17 13:46:28 +0530

    Deleted

--- Loop/06_Loops_python.py
+++
@@ -1,5 +0,0 @@
-#The provided code stub reads and integer n, For all non-negative integers i<n ,print square of i.
- n = int(input("Enter a value"))
-    for i in range (0,n):
-        print(pow(i,2))
-        i += 1
--- tuple/16_split_join.py
+++
@@ -1,8 +0,0 @@
-# to fill "-" in blank space for given string using  split and join method
-def split_and_join(line):
-        result= "-".join(line.split())
-        return result
-
-if __name__ == '__main__':
-    line = input()
-    print(split_and_join(line))
