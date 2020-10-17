5aa4d8bb683cbedb137ac56e40108b2f80c2eab2
Ajeya Nayak <ajeyanayak@gmail.com>
2020-10-17 13:36:01 +0530

    Loops

---
+++ Loop/05_loops.py
@@ -0,0 +1,5 @@
+#The provided code stub reads and integer,n,from STDIN. For all non-negative integers i<n, print i^2.
+n=int(input("Enter Input number: "))
+for i in range(n):
+    x=pow(i,2)
+    print(x) 
