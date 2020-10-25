7b4f08875e280070d50c3aefacdb01842061ffdf
Ajeya Nayak <ajeyanayak@gmail.com>
2020-10-25 15:39:22 +0530

    Arithmetic operation(division)

--- 01_introduction/04_arithmetic_operators_division_python.py
+++ 01_introduction/04_arithmetic_operators_division_python.py
@@ -1,7 +1,9 @@
 from __future__ import division
 
 if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-print(int(a / b))
-print(float(a / b))
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    if b != 0:
+        print(int(a / b))
+        print(float(a / b))
+    print(" Please Enter second number other than zero")
