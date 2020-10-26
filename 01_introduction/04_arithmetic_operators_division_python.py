3dfe41eb10a2113cece6e8e636bd30f3c5533797
Ajeya Nayak <ajeyanayak@gmail.com>
2020-10-26 11:47:01 +0530

    Updated

--- 01_introduction/04_arithmetic_operators_division_python.py
+++ 01_introduction/04_arithmetic_operators_division_python.py
@@ -3,7 +3,8 @@ from __future__ import division
 if __name__ == '__main__':
     a = int(input("Enter first number: "))
     b = int(input("Enter second number: "))
-    if b != 0:
+    while b != 0:
         print(int(a / b))
         print(float(a / b))
+    b = int(input("Enter second number: "))
     print(" Please Enter second number other than zero")
