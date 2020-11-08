c78d2628a5dbca3cc34fcbb4b540505378903e3f
Ajeya Nayak <ajeyanayak@gmail.com>
2020-11-08 14:40:28 +0530

    error regex parsing

---
+++ 13_regex_parsing/01_findall.py
@@ -0,0 +1,11 @@
+'''You are given a string S. It consists of alphanumeric characters, spaces and symbols(+,-).
+Your task is to find all the substrings of S that contains 2 or more vowels.
+Also, these substrings must lie in between 2 consonants and should contain vowels only.'''
+
+# Input Format
+'''A single line of input containing string S.'''
+
+import re
+s = '[qwrtypsdfghjklzxcvbnm]'
+a = re.findall('(?<=' + s +')([aeiou]{2,})' + s, input(), re.I)
+print('\n'.join(a or ['-1']))
