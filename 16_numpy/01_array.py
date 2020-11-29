4257859b676b86f563fb85717869c3b440288fa3
Ajeya Nayak <ajeyanayak@gmail.com>
2020-11-29 23:16:02 +0530

    Numpy array

---
+++ 16_numpy/01_array.py
@@ -0,0 +1,15 @@
+#Task
+''' You are given a space separated list of numbers.
+Your task is to print a reversed NumPy array with the element type float. '''
+
+# Input Format
+''' A single line of input containing space separated numbers.'''
+
+import numpy
+
+def arrays(arr):
+    return numpy.array(arr[::-1],float)
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
