a18be3c8016494be3ae044bccf7c75ce78bd1283
Ajeya Nayak <ajeyanayak@gmail.com>
2020-12-09 15:32:11 +0530

    Valid Credit Card

---
+++ 13_regex_parsing/12_valid_creaditcard.py
@@ -0,0 +1,23 @@
+# Task 
+''' To check whether card is valid or not. A valid credit card  has the following characteristics:
+► It must start with a 4, 5 or 6.
+► It must contain exactly 16 digits.
+► It must only consist of digits (0-9).
+► It may have digits in groups of 4, separated by one hyphen "-".
+► It must NOT use any other separator like ' ' , '_', etc.
+► It must NOT have 4 or more consecutive repeated digits.'''
+
+# Input Format
+''' The first line of input contains an integer N.
+The next N lines contain credit card numbers. '''
+
+import re
+
+
+for x in range(int(input())):
+    s = input()
+
+    if re.match(r"^[456]([\d]{15}|[\d]{3}(-[\d]{4}){3})$", s) and not re.search(r"([\d])\1\1\1", s.replace("-", "")):
+        print("Valid")
+    else:
+        print("Invalid")
